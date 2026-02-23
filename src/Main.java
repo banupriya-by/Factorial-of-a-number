7276f7a33f7b9114fae86ec0d43ca8d74748d07b
banupriya-by <bybanupriya@gmail.com>
2026-02-23 22:30:15 +0530

    Program Done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
+
+    //    using recursion
+    public int fact(int num) {
+        if (num == 0)
+            return 1;
+        return num * fact(num - 1);
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
+        int n = num;
+
+        Main m = new Main();
+        System.out.println(m.fact(n));
+
         int fact = 1;
 
         for (int i = num; i > 0; i--) {
@@ -12,5 +25,6 @@ public class Main {
             num--;
         }
         System.out.println(fact);
+
     }
 }
